d0e596ca48a211d8c758ad560bb6e9d644c68b2e
Nikitka <nikitka.bu@gmail.com>
2023-10-10 10:57:33 +0200

    Move HTTPClient to it's own file

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,15 +7,6 @@
 
 import Foundation
 
-public enum HTTPClienResult {
-    case success(Data, HTTPURLResponse)
-    case failure (Error)
-}
-
-public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClienResult) -> Void)
-}
-
 public final class RemoteFeedLoader {
     
     private let client: HTTPClient
