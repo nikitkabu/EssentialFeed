eda2b08bdcf46d9ea7780e1ceebccca96ce5da88
Nikitka <nikitka.bu@gmail.com>
2023-10-11 09:08:40 +0200

    Resumes data task on creation

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -18,7 +18,7 @@ class URLSessionHTTPClient {
         let urlRequest = URLRequest(url: url)
         session.dataTask(with: urlRequest) { _, _, _ in
             
-        }
+        }.resume()
     }
 }
 
@@ -34,20 +34,47 @@ class URLSessionHTTPClientTest: XCTestCase {
         XCTAssertEqual(session.receivedURLs, [url])
     }
     
+    func test_getFromURL_resumesDataTaskithURL() {
+        let url = URL(string: "www.onliner.by")!
+        
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
+        
+        let loader = URLSessionHTTPClient(session: session)
+        loader.get(from: url)
+                
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+
     // MARK: - Helpers
     
     private class URLSessionSpy: URLSession {
         var receivedURLs = [URL]()
+        private var stubs = [URL: URLSessionDataTask]()
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
+        }
         
         override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             if let url = request.url {
                 receivedURLs.append(url)
+                return stubs[url] ?? FakeURLSessionDataTask()
             }
             return FakeURLSessionDataTask()
         }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() { }
+    }
+    
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCallCount: Int = 0
         
-        private class FakeURLSessionDataTask: URLSessionDataTask {
-            
+        override func resume() {
+            resumeCallCount += 1
         }
     }
 }
