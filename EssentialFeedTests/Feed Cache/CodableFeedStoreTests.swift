45fae110c9019002d6f61ca9e2f7194ebec6b8d7
Nikita Vishnevsky <nikitka.bu@gmail.com>
2023-11-01 13:15:49 +0100

    Make EssentialFeed and EssentialFeedTest targets support macOS and iOS

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -111,7 +111,7 @@ final class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
     }
     
     func test_delete_deliversErrorOnDeletionError() {
-        let noDeletePermissionURL = cachesDirectory()
+        let noDeletePermissionURL = noDeletePermissionURL()
         let sut = makeSUT (storeURL: noDeletePermissionURL)
         assertThatDeleteDeliversErrorOnDeletionError(on: sut)
     }
@@ -134,6 +134,10 @@ final class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
     
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+
+    private func noDeletePermissionURL() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .systemDomainMask).first!
     }
 
     private func setupEmptyStoreState() {
